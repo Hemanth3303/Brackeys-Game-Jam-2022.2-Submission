df1fc27ca5da0a485fdf6677dee9853eb5696824
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2022-08-21 20:55:36 +0530

    shader

--- Game/src/shader.c
+++ Game/src/shader.c
@@ -5,6 +5,8 @@ void shader_init(Shader *shader, const char *vs_filename, const char *fs_filenam
 	GLuint vs_id, fs_id;
 
 	long number_of_bytes;
+	int succes;
+	char infoLog[2048];
 
 	FILE *fptr;
 	fptr=fopen(vs_filename, "r");
@@ -19,12 +21,44 @@ void shader_init(Shader *shader, const char *vs_filename, const char *fs_filenam
 	fread(fs_source, sizeof(char), number_of_bytes, fptr);
 	fclose(fptr);
 	
+	vs_id=glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs_id, 1, &vs_source, NULL);
+	glCompileShader(vs_id);
+
+	glGetShaderiv(vs_id, GL_COMPILE_STATUS, &succes);
+	if(!succes) {
+		glGetShaderInfoLog(vs_id, 2048, NULL, infoLog);
+		printf("Error: Vertex Shader Compilation Error:\n%s\n", infoLog);
+	}
 	
+	fs_id=glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs_id, 1, &fs_source, NULL);
+	glCompileShader(fs_id);
+
+	glGetShaderiv(fs_id, GL_COMPILE_STATUS, &succes);
+	if(!succes) {
+		glGetShaderInfoLog(fs_id, 2048, NULL, infoLog);
+		printf("Error: Fragment Shader Compilation Error:\n%s\n", infoLog);
+	}
+
+	shader->shader_id=glCreateProgram();
+	glAttachShader(shader->shader_id, vs_id);
+	glAttachShader(shader->shader_id, fs_id);
+	glLinkProgram(shader->shader_id);
+
+	glGetProgramiv(shader->shader_id, GL_LINK_STATUS, &succes);
+	if(!succes) {
+		glGetProgramInfoLog(shader->shader_id, 2048, NULL, infoLog);
+		printf("Error: Shader Program Linking Error:\n%s\n", infoLog);
+	}
+
+	glDeleteShader(vs_id);
+	glDeleteShader(fs_id);
 
 	free(vs_source);
 	free(fs_source);
 }
 
 void shader_use(Shader *shader) {
-
+	glUseProgram(shader->shader_id);
 }
